78dc790a8afecb7439d717c196b5fbe68026871d
Leena-MD <89838379+Leena-MD@users.noreply.github.com>
2021-10-28 18:07:06 +0300

    Update Edit_password_page.dart

--- first_swap-master/lib/src/pages/Edit_password_page.dart
+++ first_swap-master/lib/src/pages/Edit_password_page.dart
@@ -1,4 +1,3 @@
-// TODO Implement this library.import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:first_swap/models/user_model.dart';
@@ -9,6 +8,10 @@ import 'package:first_swap/src/widgets/appbar_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'profile_page.dart';
 import 'edit_profile_page.dart';
+import 'package:first_swap/fluttericon.dart';
+import 'edit_profile_page.dart';
+
+
 
 class EditPasswordPage extends StatefulWidget {
   const EditPasswordPage({Key? key}) : super(key: key);
@@ -16,51 +19,81 @@ class EditPasswordPage extends StatefulWidget {
   @override
   _EditPasswordPageState createState() => _EditPasswordPageState();
 }
+
+
 class _EditPasswordPageState  extends State<EditPasswordPage>  {
 
+  final TextEditingController pass1cont = new TextEditingController(); // القديم
 
-
-  final _auth = FirebaseAuth.instance;
-
-
-
-  final passwordEditingController = new TextEditingController();
-  final confirmPasswordEditingController = new TextEditingController();
-  String userID ="";
-
+  final passwordEditingController = new TextEditingController();//الجديد  
+  final confirmPasswordEditingController = new TextEditingController(); //اعادة الجديد
 
 
   @override
   Widget build(BuildContext context) {
-    //to change password يمكن نحتاجه
+
+var errorMsg="";
+    //password field and validation القديم 
+    final passwordFieldPlace = TextFormField(
+        autofocus: false,
+        textAlign: TextAlign.right,
+        controller: pass1cont,
+        obscureText: true,
+        validator: (value) {
+          //validation of empty password
+
+          if (value!.isEmpty) {
+            return '* حقل مطلوب';
+          } else {
+            return null;
+          }
+        },
+        onSaved: (value) {
+          pass1cont.text = value!;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          prefixIcon: new Icon(
+            FlutterIcons.lock,
+          ),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "   كلمة المرور الحالية",
+          labelStyle: TextStyle(
+            color: Colors.grey,
+            fontSize: 15,
+          ),
+        ));
+
+    //password field الجديدة
     final passwordField = TextFormField(
         textAlign: TextAlign.right,
         autofocus: false,
         controller: passwordEditingController,
         obscureText: true,
         validator: (value) {
-          //RegExp regex =
-          // new RegExp(
-          // "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+          
+       RegExp regex = new RegExp(
+             r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
           if (value!.isEmpty) {
             return ("*الحقل مطلوب");
           }
-          // if (!regex.hasMatch(value)) {
-          //  return ("يجب ان تحتوي كلمة المرور على :حرف كبير وصغير وارقام ورمز");
-          // }
+          
+    if (!regex.hasMatch(value)) {
+        return ("يجب ان تحتوي كلمة المرور على :حرف كبير وصغير وارقام ورمز");
+       }
         },
         onSaved: (value) {
-
+      //    firstNameEditingController.text = value!;
         },
         textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.lock),
           contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          hintText: "كلمة المرور",
+          hintText: "كلمة المرور الجديدة",
         ));
 
 
-//confirm password field
+    //confirm password field
     final confirmPasswordField = TextFormField(
         textAlign: TextAlign.right,
         autofocus: false,
@@ -82,38 +115,42 @@ class _EditPasswordPageState  extends State<EditPasswordPage>  {
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.lock),
           contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          hintText: "أعد إدخال كملة المرور",
+          hintText: " أعد إدخال كملة المرور الجديدة",
         ));
 
-    // final   SaveButton = Material(
-    //   elevation: 5,
-    //   borderRadius: BorderRadius.circular(30),
-    //   color: Colors.cyan[800],
-    //   child: MaterialButton(
-    //       padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-    //       minWidth: MediaQuery.of(context).size.width,
-    //
-    //       onPressed: () {
-    //         if(passwordEditingController.text.isNotEmpty&&confirmPasswordEditingController.text.isNotEmpty){
-    //
-    //           submitAction(context);}
-    //         if(passwordEditingController.text.isEmpty&&confirmPasswordEditingController.text.isEmpty){
-    //           Navigator.pop(context);
-    //         };
-    //       },
-    //       child: Text(
-    //         "حفظ التعديلات",
-    //         textAlign: TextAlign.center,
-    //         style: TextStyle(
-    //             fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
-    //       )),
-    // );
-    //
 
 
-    return Scaffold(
-      appBar: buildAppBar(context),
+    final changeButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.cyan[800],
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery.of(context).size.width,
+          onPressed: () {
+            _changePassword(pass1cont.text, passwordEditingController.text);
+          },
+          child: Text(
+            "تغيير كلمة المرور",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
 
+
+     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xff51878d)),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: Center(
         child: SingleChildScrollView(
           child: Container(
@@ -121,6 +158,7 @@ class _EditPasswordPageState  extends State<EditPasswordPage>  {
             child: Padding(
               padding: const EdgeInsets.all(36.0),
               child: Form(
+            //    key: _formKey,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -131,16 +169,14 @@ class _EditPasswordPageState  extends State<EditPasswordPage>  {
                           "assets/Screen Shot 1443-03-02 at 6.09.18 PM.png",
                           fit: BoxFit.contain,
                         )),
-                    SizedBox(height: 45),
-
-
+      passwordFieldPlace,
+                    SizedBox(height: 30),
                     passwordField,
-                    SizedBox(height: 20),
+                    SizedBox(height: 30),
                     confirmPasswordField,
-
-                    SizedBox(height: 20),
-
-
+                    SizedBox(height: 30),
+                    changeButton,
+                    SizedBox(height: 15),
                   ],
                 ),
               ),
@@ -149,55 +185,33 @@ class _EditPasswordPageState  extends State<EditPasswordPage>  {
         ),
       ),
     );
-
   }
-// updateUserData(String FirstName, String LastName, String uid) async {
-//
-//   final _auth = FirebaseAuth.instance;
-//
-//   // calling our firestore
-//   // calling our user model
-//   // updateing Data
-//   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-//   User? user = _auth.currentUser;
-//
-//   UserModel userModel = UserModel();
-//
-//   // writing  the values
-//
-//   userModel.FirstName = passwordEditingController.text;
-//
-//
-//   final firebaseUser = await FirebaseAuth.instance.currentUser;
-//   if(FirstName!=null&&LastName!=null){
-//     if (firebaseUser != null)//
-//       await FirebaseFirestore.instance
-//           .collection('users')
-//           .doc(firebaseUser.uid)
-//           .update({
-//         'FirstName': FirstName,'LastName': LastName
-//       });   }
-//
-//   userID =firebaseUser!.uid;
-//
-//   Fluttertoast.showToast(msg: "تم التعديل بنجاح");
-//
-//   Navigator.pushAndRemoveUntil(
-//       (this.context),
-//       MaterialPageRoute(builder: (context) => ProfilePage()),
-//           (route) => false);
-// }
-//
-// updateData(String FirstName, String LastName,String userID) async {
-//
-//   await updateUserData(FirstName, LastName, userID);
-//
-//
-// }
-// submitAction(BuildContext context) {
-//   updateData(firstNameEditingController.text, secondNameEditingController.text,
-//       userID);
-//   firstNameEditingController.clear();
-//   secondNameEditingController.clear();}
+void _changePassword(String currentPassword, String newPassword) async {
+  print("i am here1");
+final user = await FirebaseAuth.instance.currentUser;
+final cred = EmailAuthProvider.credential(
+    email: user!.email!, password: currentPassword);
+ print("i am here2");
+//user.reauthenticateWithCredential(credential)
+user.reauthenticateWithCredential(cred).then((value) {
+   print("i am here3");
+  user.updatePassword(newPassword).then((_) {
+      Fluttertoast.showToast(msg: "تم تغيير كلمة المرور بنجاح!");
+           //       Navigator.of(context).pushReplacement(
+             //         MaterialPageRoute(builder: (context) => HomePage())),
+     print("i am here4");
+    //Success, do something
+  }).catchError((print(error)) {
+    Fluttertoast.showToast(msg: "الرجاء التحقق من صحة المدخلات!");
+     print("i am here5");
+    //Error, show something
+  });
+}).catchError((print(err)) {
+    Fluttertoast.showToast(msg: "الرجاء التحقق من صحة المدخلات!");
+  print("i am here6");
+}
 
+);}
+
+  
 }
