3631bd428b6bf3c295ac9273ab9c47e7e77e3ed3
ShaikhaAlkanhal1 <92454583+ShaikhaAlkanhal1@users.noreply.github.com>
2022-01-25 01:35:25 +0300

    Update pet.dart

--- first_swap-master/lib/src/pages/pet.dart
+++ first_swap-master/lib/src/pages/pet.dart
@@ -487,6 +487,7 @@ class _pet extends State<pet> {
                                         cate: e.cate,
                                         owner: e.owner,
                                         IDgoods: e.IDgoods,
+                                        ownerRate: e.ownerRate,
                                       ),
                                     ),
                                   );
