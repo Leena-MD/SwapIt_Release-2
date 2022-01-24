521e4f27230a30b3d0a48776294e5895a9471a58
ShaikhaAlkanhal1 <92454583+ShaikhaAlkanhal1@users.noreply.github.com>
2022-01-25 01:39:44 +0300

    Update user_model.dart
    user rate

--- first_swap-master/lib/models/user_model.dart
+++ first_swap-master/lib/models/user_model.dart
@@ -5,7 +5,13 @@ class UserModel {
   String? phoneN;
   String? FirstName;
   String? LastName;
-  int? Rate;
+  double? Rate;
+  String? Rate1;
+  String? Rate2;
+  String? Rate3;
+  String? Rate4;
+  String? Rate5;
+  String? NumRate;
   bool? Blacklist;
 
   UserModel(
@@ -16,6 +22,12 @@ class UserModel {
       this.FirstName,
       this.LastName,
       this.Rate,
+      this.Rate1,
+      this.Rate2,
+      this.Rate3,
+      this.Rate4,
+      this.Rate5,
+      this.NumRate,
       this.Blacklist});
 
   // receiving data from server
@@ -28,6 +40,12 @@ class UserModel {
       FirstName: map['FirstName'],
       LastName: map['LastName'],
       Rate: map['Rate'],
+      Rate1: map['rate1'],
+      Rate2: map['rate2'],
+      Rate3: map['rate3'],
+      Rate4: map['rate4'],
+      Rate5: map['rate5'],
+      NumRate: map['NumRate'],
       Blacklist: map['Blacklist'],
     );
   }
@@ -42,6 +60,12 @@ class UserModel {
       'FirstName': FirstName,
       'LastName': LastName,
       'Rate': Rate,
+      'rate1': Rate1,
+      'rate2': Rate2,
+      'rate3': Rate3,
+      'rate4': Rate4,
+      'rate5': Rate5,
+      'NumRate': Rate1,
       'Blacklist': Blacklist,
     };
   }
