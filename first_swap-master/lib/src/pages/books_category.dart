b5ef4ea799643fab338200c75d501032200257c4
ShaikhaAlkanhal1 <92454583+ShaikhaAlkanhal1@users.noreply.github.com>
2022-01-25 01:25:05 +0300

    Update books_category.dart
    owner rate

--- first_swap-master/lib/src/pages/books_category.dart
+++ first_swap-master/lib/src/pages/books_category.dart
@@ -487,6 +487,7 @@ class _BooksCat extends State<BooksCat> {
                                         cate: e.cate,
                                         owner: e.owner,
                                         IDgoods: e.IDgoods,
+                                        ownerRate: e.ownerRate,
                                       ),
                                     ),
                                   );
