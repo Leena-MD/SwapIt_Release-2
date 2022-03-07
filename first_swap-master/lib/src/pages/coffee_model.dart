8cdb582a10927077144c09a729b246741ad91eab
abeerkn <78116485+abeerkn@users.noreply.github.com>
2022-03-07 17:41:53 +0300

    Update coffee_model.dart

--- first_swap-master/lib/src/pages/coffee_model.dart
+++ first_swap-master/lib/src/pages/coffee_model.dart
@@ -17,40 +17,145 @@ class Coffee {
 
 final List<Coffee> coffeeShops = [
   Coffee(
-      shopName: 'Stumptown Coffee Roasters',
-      address: '18 W 29th St',
+      shopName: 'وقف السلام الخيري',
+      address: 'طريق عبدالرحمن بن عوف - غرب مقبرة النسيم',
       description:
           'Coffee bar chain offering house-roasted direct-trade coffee, along with brewing gear & whole beans',
       locationCoords: LatLng(40.745803, -73.988213),
       thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNNzoa4RVMeOisc0vQ5m3Z7aKet5353lu0Aah0a=w90-h90-n-k-no'
       ),
   Coffee(
-      shopName: 'Andrews Coffee Shop',
-      address: '463 7th Ave',
+      shopName: 'جمعية البر حي الفيحاء',
+      address: 'شارع ابو عبيدة بن الجراح ',
       description:
-          'All-day American comfort eats in a basic diner-style setting',
+          '',
       locationCoords: LatLng(40.751908, -73.989804),
       thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipOfv3DSTkjsgvwCsUe_flDr4DBXneEVR1hWQCvR=w90-h90-n-k-no'
       ),
   Coffee(
-      shopName: 'Third Rail Coffee',
-      address: '240 Sullivan St',
+      shopName: 'جمعية البر حي الفيصلية',
+      address: 'مخرج 17 - شارع أسد السنة',
       description:
           'Small spot draws serious caffeine lovers with wide selection of brews & baked goods.',
       locationCoords: LatLng(40.730148, -73.999639),
       thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipPGoxAP7eK6C44vSIx4SdhXdp78qiZz2qKp8-o1=w90-h90-n-k-no'
       ),
   Coffee(
-      shopName: 'Hi-Collar',
-      address: '214 E 10th St',
+      shopName: 'جمعية البر - حي الربوة',
+      address: 'طريق مكة المكرمة الفرعي',
       description:
           'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
       locationCoords: LatLng(40.729515, -73.985927),
       thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
       ),
   Coffee(
-      shopName: 'Everyman Espresso',
-      address: '301 W Broadway',
+      shopName: 'جمعية البر - حي العزيزية',
+      address: 'شرق جامع البواردي',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - حي الروضة والجنادرية',
+      address: 'طريق خالد بن الوليد - شمال طريق الملك عبدالله - حي الحمراء',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - حي معكال والشميسي',
+      address: 'شارع الأمير عبدالله بن عبدالرحمن(الحجاز) - خلف أسواق القدس',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - شارع السويدي',
+      address: 'شارع السويدي العام - مقابل العثيم',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - حي الشفا',
+      address: 'شارع كربلاء - شرق جامع الأميرة سارة',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - حي الحزم',
+      address: 'شارع المشتل - خلف الدفاع المدني',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - حي العريجاء',
+      address: 'شارع العريجاء العام - جوار جامع العريجاء',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - شارع حمزة بن عبدالمطلب',
+      address: 'طريق حمزة بن عبدالمطلب - جوار جامع آل فريان - شبرا',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - فرع غرب الرياض',
+      address: 'طريق المدينة المنورة - غرب سوق الخضار',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية عرقة الخيرية',
+      address: 'شارع الأمير مشعل بن عبدالعزيز',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - شمال الرياض',
+      address: 'طريق الملك عبدالله - الورود',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية الإمام محمد بن سعود الخيرية',
+      address: 'الامام تركي بن عبدالله - الطريف الجديد - الدرعية',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+  Coffee(
+      shopName: 'جمعية البر - حي الملك فهد',
+      address: 'شارع هشام بن عبدالملك - شمال حياة مول',
+      description:
+      'Snazzy, compact Japanese cafe showcasing high-end coffee & sandwiches, plus sake & beer at night.',
+      locationCoords: LatLng(40.729515, -73.985927),
+      thumbNail: 'https://lh5.googleusercontent.com/p/AF1QipNhygtMc1wNzN4n6txZLzIhgJ-QZ044R4axyFZX=w90-h90-n-k-no'
+  ),
+
+  Coffee(
+      shopName: 'جمعية البر - حي الخالدية',
+      address: 'طريق الأمير محمد بن عبدالرحمن',
       description:
           'Compact coffee & espresso bar turning out drinks made from direct-trade beans in a low-key setting.',
       locationCoords: LatLng(40.721622, -74.004308),
